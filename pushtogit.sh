cfa8fd32cf2e86581730a41bb0d89e11c5881faa
nvdinh185 <nvdinh0905671805@gmail.com>
2019-06-07 09:59:22 +0700

    first commit

--- pushtogit.sh
+++ pushtogit.sh
@@ -1,5 +1,5 @@
-git init
+#git init
 git add .
 git commit -m "first commit"
-git remote add gitdinh https://github.com/nvdinh185/node-infinite-refresh.git
-git push gitdinh
+#git remote add gitdinh https://github.com/nvdinh185/node-infinite-refresh.git
+git push gitdinh master
